b57f3f0f47ca664ba663041f1568ca43faf7140f
David Olowomeye <olowomeyedavid242@gmail.com>
2023-12-12 18:11:51 +0000

    Implemented User Registration and Login feature

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+// To import the models
+const UserModel = require("./models/User");
+
+const app = express();
+
+// To encrypt the password
+const bcryptSalt = bcrypt.genSaltSync(10);
+// For JWT
+const jwtSecret = "ioujhyt67d89ew0iowjhgytfghsjdwiofv897we";
 
 // MIDDLEWARES
 app.use(express.json());
@@ -23,9 +33,45 @@ app.get("/test", (req, res) => {
 });
 
 // For user registration
-app.post("/register", (req, res) => {
+app.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  res.json({ name, email, password });
+  try {
+    const registeredUserDoc = await UserModel.create({
+      name,
+      email,
+      password: bcrypt.hashSync(password, bcryptSalt),
+    });
+
+    res.json(registeredUserDoc);
+  } catch (err) {
+    alert(err);
+    res.status(422).json(err);
+  }
+});
+
+// For user Login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const loggedInUserDoc = await UserModel.findOne({ email });
+
+  if (loggedInUserDoc) {
+    const passwordOk = bcrypt.compareSync(password, loggedInUserDoc.password);
+    if (passwordOk) {
+      jwt.sign(
+        { email: loggedInUserDoc.email, id: loggedInUserDoc._id },
+        jwtSecret,
+        {},
+        (err, token) => {
+          if (err) throw err;
+          res.cookie("token", token).json("Password is correct!");
+        }
+      );
+    } else {
+      res.status(422).json("Password is not correct!");
+    }
+  } else {
+    res.json("User not found!");
+  }
 });
 
 app.listen(4000, () => {
